554d885a0ab76f98e7ef4dba3bd3819a85f8c654
misk2 <117576468+misk2@users.noreply.github.com>
2024-12-05 12:34:20 +0100

    Update settings.py

--- DjangoKursy/settings.py
+++ DjangoKursy/settings.py
@@ -146,4 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_HOST_USER = 'kapipvp@gmail.com'
+EMAIL_HOST_PASSWORD = 'uevr xheg aiua bmmt'
